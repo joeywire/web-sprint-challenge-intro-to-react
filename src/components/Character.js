f7b5951cbf850bb75c8c305396048332db37ff40
Joe L <joseph.lasata@gmail.com>
2020-10-09 10:36:53 -0500

    API set up, App state established, props being sent down correctly to Character

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
 // Write your Character component here
+export default function Char(props) {
+    const {key, character} = props;
+    
+    return (
+        <div>
+            <p>Character: {character.name}</p>
+        </div>
+    )
+
+}
